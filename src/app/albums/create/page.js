7fd2aab47f9518d829c4b011a94bef5d6ccb6e82
iddreeek <159159274+iddreeek@users.noreply.github.com>
2025-01-18 16:53:21 +0800

    update create/page.js

--- src/app/albums/create/page.js
+++ src/app/albums/create/page.js
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Textarea } from '@/components/ui/textarea'
-import { useToast } from '@/components/ui/use-toast'
+import { useToast } from '@/hooks/use-toast'
 
 export default function CreateAlbum() {
   const [title, setTitle] = useState('')
