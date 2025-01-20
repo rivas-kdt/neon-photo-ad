65d6aaade96e6038d3ef27f6d274ced629eaf83c
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-21 07:55:01 +0800

    try

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -6,47 +6,28 @@ import { neon } from "@neondatabase/serverless";
 
 export async function POST(request) {
   try {
-    const sql = neon(process.env.DATABASE_URL);
+      const sql = neon(process.env.DATABASE_URL);
     const { email, password } = await request.json();
 
     const users = await sql`SELECT * FROM users WHERE email = ${email}`;
 
     if (users.length === 0) {
-      return NextResponse.json(
-        { error: "Invalid credentials" },
-        { status: 401 }
-      );
+      return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
     }
 
     const user = users[0];
     const passwordMatch = await bcrypt.compare(password, user.password_hash);
 
     if (!passwordMatch) {
-      return NextResponse.json(
-        { error: "Invalid credentials" },
-        { status: 401 }
-      );
+      return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
     }
 
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    const res = NextResponse.json({ message: "Login successful" });
-
-    res.cookies().set("token", token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60, // 1 week
-      path: "/",
-    });
-    return res;
+    return NextResponse.json({ token: token, user: { id: user.id, username: user.username, email: user.email } });
   } catch (error) {
     console.error("Error logging in:", error);
-    return NextResponse.json(
-      { error: "Internal Server Error" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
   }
 }
+
