a49cc9c0c364b02bb18f3968aa2428d636c57964
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-21 09:17:15 +0800

    try

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,12 +2,21 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Image, GalleryVerticalEnd, User, Home, Upload, LogIn, UserPlus } from "lucide-react";
+import {
+  Image,
+  GalleryVerticalEnd,
+  User,
+  Home,
+  Upload,
+  LogIn,
+  UserPlus,
+} from "lucide-react";
 import { useAuth } from "@/hooks/useAuth";
 
 const Navbar = () => {
-  const { user, loading } = useAuth()
+  const { user, loading } = useAuth();
   const pathname = usePathname();
+  console.log(user);
 
   const navItems = [
     { href: "/", icon: Home, label: "Home" },
@@ -18,39 +27,41 @@ const Navbar = () => {
   ];
 
   const navItems2 = [
-    { href: "/login", icon: LogIn, label: "Login"},
-    { href: "/register", icon: UserPlus, label: "Register"},
-  ]
+    { href: "/login", icon: LogIn, label: "Login" },
+    { href: "/register", icon: UserPlus, label: "Register" },
+  ];
 
   return (
     <nav className="fixed bottom-0 left-0 right-0 h-14 bg-white border-t border-gray-200 z-10">
       <div className="container mx-auto px-4 h-full">
         <ul className="flex justify-around items-center h-full">
-          { user ? navItems.map((item) => (
-            <li key={item.href}>
-              <Link
-                href={item.href}
-                className={`flex flex-col items-center ${
-                  pathname === item.href ? "text-blue-500" : "text-gray-500"
-                }`}
-              >
-                <item.icon className="h-6 w-6" />
-                <span className="text-xs mt-1">{item.label}</span>
-              </Link>
-            </li>
-          )) : navItems2.map((item) => (
-            <li key={item.href}>
-              <Link
-                href={item.href}
-                className={`flex flex-col items-center ${
-                  pathname === item.href ? "text-blue-500" : "text-gray-500"
-                }`}
-              >
-                <item.icon className="h-6 w-6" />
-                <span className="text-xs mt-1">{item.label}</span>
-              </Link>
-            </li>
-          ))}
+          {user
+            ? navItems.map((item) => (
+                <li key={item.href}>
+                  <Link
+                    href={item.href}
+                    className={`flex flex-col items-center ${
+                      pathname === item.href ? "text-blue-500" : "text-gray-500"
+                    }`}
+                  >
+                    <item.icon className="h-6 w-6" />
+                    <span className="text-xs mt-1">{item.label}</span>
+                  </Link>
+                </li>
+              ))
+            : navItems2.map((item) => (
+                <li key={item.href}>
+                  <Link
+                    href={item.href}
+                    className={`flex flex-col items-center ${
+                      pathname === item.href ? "text-blue-500" : "text-gray-500"
+                    }`}
+                  >
+                    <item.icon className="h-6 w-6" />
+                    <span className="text-xs mt-1">{item.label}</span>
+                  </Link>
+                </li>
+              ))}
         </ul>
       </div>
     </nav>
