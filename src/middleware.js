8e866c6dc3fecdf5acd22f25f57936dcd77ac13a
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-21 08:48:21 +0800

    try

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,16 @@
 import { NextResponse } from "next/server";
 import { getTokenFromCookie, verifyToken } from "./lib/auth";
+import { useAuth } from "./hooks/useAuth";
 
 export function middleware(request) {
   const token = getTokenFromCookie();
   const userId = verifyToken(token);
-  console.log({token: token, uid: userId})
+  const {user} = useAuth
+  console.log({token: token, uid: userId, user: user})
   const publicPaths = ["/login", "/register"];
 
   if (!publicPaths.includes(request.nextUrl.pathname)) {
   }
-
   return NextResponse.next();
 }
 
