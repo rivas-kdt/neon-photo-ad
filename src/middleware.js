b9a5220c826bc722d97cd7a0aac8b1a5d0a909d9
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-21 09:54:47 +0800

    try

--- src/middleware.js
+++ src/middleware.js
@@ -2,8 +2,8 @@ import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const { token } = cookies().get("token");
-  console.log(token.value);
+  const token = cookies().get("token");
+  console.log(token);
   const publicPaths = ["/login", "/register"];
 
   if (!publicPaths.includes(request.nextUrl.pathname)) {
