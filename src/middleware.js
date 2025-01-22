287d6e8b48723c1296a158697b76acea47fafea0
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-23 07:15:41 +0800

    sessionprovider.js fixes

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,8 @@
-import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const cookieStore = cookies();
-  const token = cookieStore.get("jwt");
-  console.log({ mid: cookieStore, mid2: token });
+  const token = request.cookies["_vercel_jwt"];
+  console.log({ mid: request.cookies, mid2: token });
   const publicPaths = ["/login", "/register"];
 
   if (!publicPaths.includes(request.nextUrl.pathname)) {
