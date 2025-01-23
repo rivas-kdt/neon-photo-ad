215c90d9ed0378cb43ec0ca17a5856308f2e0dc4
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-24 07:48:43 +0800

    Fixes login withing session.js

---
+++ src/app/cookie/page.js
@@ -0,0 +1,11 @@
+import { cookies } from 'next/headers'
+ 
+export default async function Page() {
+  const cookieStore = await cookies()
+  return cookieStore.getAll().map((cookie) => (
+    <div key={cookie.name}>
+      <p>Name: {cookie.name}</p>
+      <p>Value: {cookie.value}</p>
+    </div>
+  ))
+}
