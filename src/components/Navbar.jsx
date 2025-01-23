88b64cf64b6137374ec7121ca17ee6cfd3b20d77
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-24 06:24:28 +0800

    Navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,39 +22,39 @@ const Navbar = () => {
     return <div>loading</div>;
   }
 
-  if (!user) {
-    return null;
-  }
-
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
-      <div className="container mx-auto px-4">
-        <ul className="flex justify-around py-2">
-          {navItems.map((item) => (
-            <li key={item.href}>
-              <Link
-                href={item.href}
-                className={`flex flex-col items-center ${
-                  pathname === item.href ? "text-blue-500" : "text-gray-500"
-                }`}
-              >
-                <item.icon className="h-6 w-6" />
-                <span className="text-xs mt-1">{item.label}</span>
-              </Link>
+    !loading && (
+      <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200">
+        <div className="container mx-auto px-4">
+          <ul className="flex justify-around py-2">
+            {navItems.map((item) => (
+              <li key={item.href}>
+                <Link
+                  href={item.href}
+                  className={`flex flex-col items-center ${
+                    pathname === item.href ? "text-blue-500" : "text-gray-500"
+                  }`}
+                >
+                  <item.icon className="h-6 w-6" />
+                  <span className="text-xs mt-1">{item.label}</span>
+                </Link>
+              </li>
+            ))}
+            <li>
+              {user && (
+                <button
+                  onClick={logout}
+                  className="flex flex-col items-center text-red-500"
+                >
+                  <LogOut className="h-6 w-6"></LogOut>
+                  <span className="text-xs mt-1">Logout</span>
+                </button>
+              )}
             </li>
-          ))}
-          <li>
-            <button
-              onClick={logout}
-              className="flex flex-col items-center text-red-500"
-            >
-              <LogOut className="h-6 w-6"></LogOut>
-              <span className="text-xs mt-1">Logout</span>
-            </button>
-          </li>
-        </ul>
-      </div>
-    </nav>
+          </ul>
+        </div>
+      </nav>
+    )
   );
 };
 
