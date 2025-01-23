de948e89421c49865390d5b28ea27d07e09389a9
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-24 07:32:13 +0800

    Create Context define

--- src/app/(auth)/profile/page.js
+++ src/app/(auth)/profile/page.js
@@ -4,8 +4,8 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { useSession } from "@/lib/SessionProvider";
 import { LogOut } from "lucide-react";
+import { useSession } from "@/lib/Session";
 
 export default function ProfilePage() {
   const { user, loading, logout } = useSession();
