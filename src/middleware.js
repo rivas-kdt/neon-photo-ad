e5cd11acb4ebb23b18d8d9609ae0b44932a8a9ca
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-24 12:26:53 +0800

    middleware try

--- src/middleware.js
+++ src/middleware.js
@@ -4,7 +4,9 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
   const cookieStore = await cookies();
   const jwt = cookieStore.get("_vercel_jwt");
+  const token = jwt?.value
   console.log(jwt);
+  console.log("JWT Token:", token)
 
   return NextResponse.next();
 }
