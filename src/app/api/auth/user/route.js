093e5e8a4b60a0aaa2f445e404639fde77d49205
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-24 14:59:08 +0800

    api route for auth user

--- src/app/api/auth/user/route.js
+++ src/app/api/auth/user/route.js
@@ -1,4 +1,4 @@
-import { getTokenFromCookie } from "@/lib/lib";
+import { getTokenFromCookie, verifyToken } from "@/lib/lib";
 import { neon } from "@neondatabase/serverless";
 import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
@@ -6,17 +6,17 @@ import jwt from "jsonwebtoken";
 export async function GET(req) {
   try {
     const sql = neon(process.env.DATABASE_URL);
-    const cookie = req.cookies["jwt"];
-    const id = getTokenFromCookie()
-    console.log(id)
+    const cookie = getTokenFromCookie();
+    const user_id = verifyToken(cookie);
+    console.log(cookie);
+    console.log(user_id);
     if (!cookie) {
       return NextResponse.json("Unauthorized", { status: 401 });
     }
-    const claims = jwt.verify(cookie, process.env.JWT_SECRET);
-    if (!claims) {
+    if (!user_id) {
       return NextResponse.json("Unauthenticated", { status: 401 });
     }
-    const user = await sql`SELECT * FROM users WHERE id=${claims.id}`;
+    const user = await sql`SELECT * FROM users WHERE id=${user_id}`;
     const { password_hash, ...data } = user[0];
     return NextResponse.json(data, { status: 401 });
   } catch (error) {
