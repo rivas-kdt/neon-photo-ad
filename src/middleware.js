2450cf877cac706bb7463742139e54506c78eea9
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-24 12:37:52 +0800

    middleware try

--- src/middleware.js
+++ src/middleware.js
@@ -4,9 +4,15 @@ import { NextResponse } from "next/server";
 export async function middleware(request) {
   const cookieStore = await cookies();
   const jwt = cookieStore.get("_vercel_jwt");
-  const token = jwt?.value
+  const token = jwt?.value;
   console.log(jwt);
-  console.log("JWT Token:", token)
+  console.log("JWT Token:", token);
+
+  const cookieStore2 = await cookies();
+  const jwt2 = cookieStore2.get("jwt");
+  const token2 = jwt2?.value;
+  console.log(jwt);
+  console.log("JWT Token2:", token2);
 
   return NextResponse.next();
 }
