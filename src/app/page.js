7ee068fd4a3dd052ec786dae48417f1ee5a2679f
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-27 07:54:18 +0800

    get vercel_jwt token from cookies

--- src/app/page.js
+++ src/app/page.js
@@ -8,41 +8,36 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Plus, Album } from "lucide-react";
 import { useToast } from "@/hooks/use-toast";
 import RecentPhotos from "@/components/RecentPhotos";
+import { useSession } from "@/lib/Session";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   const [albums, setAlbums] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const { toast } = useToast();
+  const { user, loading } = useSession();
+
+  if (!loading && !user) {
+    return redirect("/login");
+  }
 
   useEffect(() => {
     const fetchAlbums = async () => {
-      try {
-        const response = await fetch("https://express-api-tawny-alpha.vercel.app/api/albums/user",
-          {
-            method: "GET",
-            credentials: 'include',
-          });
-        if (response.ok) {
-          const data = await response.json();
-          setAlbums(data);
-        } else {
-          throw new Error("Failed to fetch albums");
+      const response = await fetch(
+        "https://express-api-tawny-alpha.vercel.app/api/albums/user",
+        {
+          method: "GET",
+          credentials: "include",
         }
-      } catch (error) {
-        console.error("Error fetching albums:", error);
-        toast({
-          title: "Error",
-          description: "Failed to load albums. Please try again.",
-          variant: "destructive",
-        });
-      } finally {
-        setIsLoading(false);
-      }
+      );
+      const data = await response.json();
+      setAlbums(data);
+      setIsLoading(false);
     };
 
     fetchAlbums();
   }, [toast]);
-  console.log(albums)
+  console.log(albums);
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -74,25 +69,26 @@ export default function Home() {
           </Link>
         </div>
         <div className="grid grid-cols-2 gap-4">
-          {albums && albums.map((album) => (
-            <Link href={`/albums/${album.id}`} key={album.id}>
-              <Card>
-                <CardContent className="p-4">
-                  <Image
-                    src={album.cover_photo_url || '/placeholder.webp'}
-                    alt={album.title}
-                    width={150}
-                    height={150}
-                    className="w-full h-32 object-cover rounded-md mb-2"
-                  />
-                  <h3 className="font-semibold">{album.title}</h3>
-                  <p className="text-sm text-gray-500">
-                    {new Date(album.created_at).toLocaleDateString()}
-                  </p>
-                </CardContent>
-              </Card>
-            </Link>
-          ))}
+          {albums &&
+            albums.map((album) => (
+              <Link href={`/albums/${album.id}`} key={album.id}>
+                <Card>
+                  <CardContent className="p-4">
+                    <Image
+                      src={album.cover_photo_url || "/placeholder.webp"}
+                      alt={album.title}
+                      width={150}
+                      height={150}
+                      className="w-full h-32 object-cover rounded-md mb-2"
+                    />
+                    <h3 className="font-semibold">{album.title}</h3>
+                    <p className="text-sm text-gray-500">
+                      {new Date(album.created_at).toLocaleDateString()}
+                    </p>
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
         </div>
       </section>
     </div>
