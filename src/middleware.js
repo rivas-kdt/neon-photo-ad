cfcad8029b91c5c6df7e411619fd45fb59b91292
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2025-01-27 08:52:44 +0800

    get vercel_jwt token from cookies

--- src/middleware.js
+++ src/middleware.js
@@ -3,11 +3,9 @@ import axios from "axios";
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function getCookie() {
-  const response = axios.get(`${process.env.API_URL}/auth/cookie`, {
-    withCredentials: true,
-  });
+  const response = axios.get(`${process.env.API_URL}/auth/cookie`);
   await wait(10000);
-  return response;
+  return response.data;
 }
 
 export async function middleware(request, context) {
